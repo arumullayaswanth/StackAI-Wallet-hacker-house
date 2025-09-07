4117b47a6e9c18072c9555aaeffe0cbbbd4e8038
Arumulla.yaswanth reddy <yaswanth.arumulla@gmail.com>
2025-09-07 18:45:33 +0000

    Daily Market Movers this remove in Dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import { WalletOverview } from '@/components/wallet-overview';
 import { ChatInterface } from '@/components/chat-interface';
 import { TransactionHistory } from '@/components/transaction-history';
 import { MyAssets } from '@/components/my-assets';
-import { DailyStockReport } from '@/components/daily-stock-report';
 
 export default function Home() {
   return (
@@ -12,7 +11,6 @@ export default function Home() {
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
           <div className="lg:col-span-2 space-y-8">
             <ChatInterface />
-            <DailyStockReport />
           </div>
           <div className="flex flex-col gap-8">
             <MyAssets />
